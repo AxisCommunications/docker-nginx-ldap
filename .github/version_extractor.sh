e3b3df7213997f63efbb45ad4e313c4e8faaa2be
jonas <jonas.alfredsson@protonmail.com>
2023-05-05 17:36:47 +0200

    Use 'GITHUB_ENV' instead of 'set-output'

--- .github/version_extractor.sh
+++ .github/version_extractor.sh
@@ -12,13 +12,10 @@ nginx_minor=$(sed -n -r -e 's/^FROM nginx:[1-9]\.([0-9]+)\.[0-9]+-alpine.*$/\1/p
 nginx_patch=$(sed -n -r -e 's/^FROM nginx:[1-9]\.[0-9]+\.([0-9]+)-alpine.*$/\1/p' "${1}")
 
 if [ -n "${nginx_major}" -a -n "${nginx_minor}" -a -n "${nginx_patch}" ]; then
-    echo "::set-output name=NGINX_MAJOR::${nginx_major}"
-    echo "::set-output name=NGINX_MINOR::${nginx_minor}"
-    echo "::set-output name=NGINX_PATCH::${nginx_patch}"
-else
-    echo "Could not extract all expected values:"
     echo "NGINX_MAJOR=${nginx_major}"
     echo "NGINX_MINOR=${nginx_minor}"
     echo "NGINX_PATCH=${nginx_patch}"
+else
+    >&2 echo "Could not extract all expected values: v${nginx_major}.${nginx_minor}.${nginx_patch}"
     exit 1
 fi
